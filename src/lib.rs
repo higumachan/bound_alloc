0fe5d237d8b337a52ef094a0bf65e487d2e1675d
higumachan <harekumo792154@gmail.com>
2022-04-26 20:42:08 +0900

    static変数を利用しないようにする

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,12 @@
 extern crate core;
 
-use std::{alloc::GlobalAlloc, sync::atomic::AtomicUsize};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::atomic::Ordering::SeqCst;
-use std::thread::ThreadId;
-use std::thread;
-use std::time::Duration;
 use once_cell::sync::OnceCell;
-
-
-static CURRENT: AtomicUsize = AtomicUsize::new(0);
-static PEAK: AtomicUsize = AtomicUsize::new(0);
-static BOUND_OVERFLOWED: AtomicBool = AtomicBool::new(false);
-static OVERFLOW_HANDLER_THREAD: OnceCell<ThreadId> = OnceCell::new();
+use std::sync::atomic::Ordering::SeqCst;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
+use std::thread::ThreadId;
+use std::time::Duration;
+use std::{alloc::GlobalAlloc, sync::atomic::AtomicUsize};
 
 type OverflowHandler = dyn Fn(usize) + Send + Sync;
 
@@ -20,11 +14,27 @@ struct BoundAlloc<Alloc> {
     alloc: Alloc,
     bound_size: usize,
     overflow_handler: Option<Box<OverflowHandler>>,
+    current: AtomicUsize,
+    peak: AtomicUsize,
+    bound_overflowed: AtomicBool,
+    overflow_handler_thread: OnceCell<ThreadId>,
 }
 
 impl<Alloc> BoundAlloc<Alloc> {
-    pub fn new(alloc: Alloc, bound_size: usize, overflow_handler: Option<Box<OverflowHandler>>) -> Self {
-        Self { alloc, bound_size, overflow_handler }
+    pub fn new(
+        alloc: Alloc,
+        bound_size: usize,
+        overflow_handler: Option<Box<OverflowHandler>>,
+    ) -> Self {
+        Self {
+            alloc,
+            bound_size,
+            overflow_handler,
+            current: AtomicUsize::new(0),
+            peak: AtomicUsize::new(0),
+            bound_overflowed: AtomicBool::new(false),
+            overflow_handler_thread: OnceCell::new(),
+        }
     }
 }
 
@@ -45,24 +55,31 @@ unsafe impl<Alloc: GlobalAlloc> GlobalAlloc for BoundAlloc<Alloc> {
     unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
         let ret = self.alloc.alloc(layout);
 
-        if BOUND_OVERFLOWED.load(Ordering::SeqCst) && OVERFLOW_HANDLER_THREAD.get().map(|id| thread::current().id() == *id).unwrap_or(false) {
+        if self.bound_overflowed.load(Ordering::SeqCst)
+            && self
+                .overflow_handler_thread
+                .get()
+                .map(|id| thread::current().id() == *id)
+                .unwrap_or(false)
+        {
             sleep_eternally();
         }
 
         if !ret.is_null() {
-            CURRENT.fetch_add(layout.size(), Ordering::SeqCst);
-            PEAK.fetch_max(CURRENT.load(Ordering::SeqCst), Ordering::SeqCst);
+            self.current.fetch_add(layout.size(), Ordering::SeqCst);
+            self.peak
+                .fetch_max(self.current.load(Ordering::SeqCst), Ordering::SeqCst);
         }
 
-        if self.bound_size < PEAK.load(Ordering::SeqCst) {
-            BOUND_OVERFLOWED.store(true, Ordering::SeqCst);
-            match OVERFLOW_HANDLER_THREAD.set(thread::current().id()) {
+        if self.bound_size < self.peak.load(Ordering::SeqCst) {
+            self.bound_overflowed.store(true, Ordering::SeqCst);
+            match self.overflow_handler_thread.set(thread::current().id()) {
                 Ok(_) => {
                     if let Some(handler) = &self.overflow_handler {
-                        handler(PEAK.load(Ordering::SeqCst));
+                        handler(self.peak.load(Ordering::SeqCst));
                         panic!("overflow handler must be panic");
                     } else {
-                        default_overflow_handler(PEAK.load(Ordering::SeqCst));
+                        default_overflow_handler(self.peak.load(Ordering::SeqCst));
                     }
                 }
                 Err(_) => {
@@ -76,19 +93,30 @@ unsafe impl<Alloc: GlobalAlloc> GlobalAlloc for BoundAlloc<Alloc> {
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
         self.dealloc(ptr, layout);
-        CURRENT.fetch_add(layout.size(), Ordering::SeqCst);
+        self.current.fetch_add(layout.size(), Ordering::SeqCst);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::alloc::{GlobalAlloc, Layout, System};
     use crate::BoundAlloc;
+    use std::alloc::{GlobalAlloc, Layout, System};
 
     #[test]
     #[should_panic(expected = "overflow(default overflow handler)")]
     fn it_works() {
         let alloc = BoundAlloc::new(System, 1024, None);
+
+        unsafe {
+            alloc.alloc(Layout::from_size_align(512, 8).unwrap());
+            alloc.alloc(Layout::from_size_align(512, 8).unwrap());
+            alloc.alloc(Layout::from_size_align(512, 8).unwrap());
+        }
+    }
+
+    #[test]
+    fn it_not_works() {
+        let alloc = BoundAlloc::new(System, 2048, None);
 
         unsafe {
             alloc.alloc(Layout::from_size_align(512, 8).unwrap());
